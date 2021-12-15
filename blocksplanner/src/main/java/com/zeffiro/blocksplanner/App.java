6977e48a920e345e923d756ea924cb274e5b7e5f
zeffiro <r.zinni@wordpower.it>
2021-12-15 12:11:21 +0100

    Moved population status methods to Solver class

--- blocksplanner/src/main/java/com/zeffiro/blocksplanner/App.java
+++ blocksplanner/src/main/java/com/zeffiro/blocksplanner/App.java
@@ -1,8 +1,5 @@
 package com.zeffiro.blocksplanner;
 
-import com.zeffiro.blocks.*;
-import com.zeffiro.exceptions.IllegalBlockException;
-
 /**
  * Hello world!
  *
@@ -10,61 +7,13 @@ import com.zeffiro.exceptions.IllegalBlockException;
 public class App 
 {
 
-    public TableStatus initialStatus;
-    public TableStatus finalStatus;
-
     public static void main( String[] args )
     {
         
-    }
-
-    public void populateInitial() {
-
-        Block a = new Block("A");
-        Block b = new Block("B");
-        Block c = new Block("C");
-        Block d = new Block("D");
-
-        a.setOver(b);
-        b.setOver(c);
-
-        this.initialStatus = new TableStatus();
-
-        try {
-            this.initialStatus.addBlock(a);
-            this.initialStatus.addBlock(b);
-            this.initialStatus.addBlock(c);
-            this.initialStatus.addBlock(d);
-        }
-        catch(IllegalBlockException ex) {
-            System.out.println("Impossibile popolare lo stato iniziale: "+ex.getMessage());
-        }
+        Solver solver = new Solver();
+        solver.solve();
 
     }
 
-    public void populateFinal() {
-
-        Block a = new Block("A");
-        Block b = new Block("B");
-        Block c = new Block("C");
-        Block d = new Block("D");
-
-        d.setOver(c);
-        c.setOver(b);
-        b.setOver(a);
-
-        this.finalStatus = new TableStatus();
-
-        try {
-            this.finalStatus.addBlock(a);
-            this.finalStatus.addBlock(b);
-            this.finalStatus.addBlock(c);
-            this.finalStatus.addBlock(d);
-        }
-        catch(IllegalBlockException ex) {
-            System.out.println("Impossibile popolare lo stato finale: "+ex.getMessage());
-        }
-
-    }
 
 }
