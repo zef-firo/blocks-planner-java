6a607f2cbfb54074c30d98e32967f9a29da69b9b
zeffiro <r.zinni@wordpower.it>
2021-12-16 00:50:32 +0100

    Exception for operation not permitted on block

---
+++ blocksplanner/src/main/java/com/zeffiro/exceptions/CannotDoOperationException.java
@@ -0,0 +1,7 @@
+package com.zeffiro.exceptions;
+
+public class CannotDoOperationException extends Exception {
+    public CannotDoOperationException(String eMessage) {
+        super(eMessage);
+    }
+}
