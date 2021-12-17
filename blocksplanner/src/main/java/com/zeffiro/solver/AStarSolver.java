2865e3b32ffbac23d1b20c3dbd4d8907b4211480
zeffiro <r.zinni@wordpower.it>
2021-12-17 19:40:57 +0100

    Added Astar algo

---
+++ blocksplanner/src/main/java/com/zeffiro/solver/AStarSolver.java
@@ -0,0 +1,133 @@
+package com.zeffiro.solver;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+import com.zeffiro.blocks.Block;
+import com.zeffiro.blocks.TableStatus;
+import com.zeffiro.exceptions.CannotDoOperationException;
+import com.zeffiro.operations.Operation;
+import com.zeffiro.tree.WeightedNode;
+
+public class AStarSolver extends Solver {
+
+    private WeightedNode solverTree;
+    private WeightedNode solutionLeaf;
+    private PriorityQueue<WeightedNode> toExplore;
+
+    public AStarSolver() {
+        super();
+        this.toExplore = new PriorityQueue<WeightedNode>();
+    }
+
+    public void solve() {
+        super.solve();
+
+        this.solutionLeaf = null;
+
+        //set tree root
+        this.solverTree = new WeightedNode(this.initialStatus, 0);
+        this.toExplore.add(this.solverTree);
+
+        WeightedNode head = this.toExplore.poll();
+        while (this.solutionLeaf == null && head != null) {
+            this.performSolution(head);
+            head = this.toExplore.poll();
+        }
+
+        //print solution
+        if (this.solutionLeaf == null) {
+            System.out.println("No solution found for the arrangment.");
+        }
+        else {
+            System.out.println("The solution is:\r\n");
+            this.printSolution(this.solutionLeaf);
+        }
+
+    }
+
+    private void printSolution(WeightedNode n) {
+
+        if (n.getParent() != null) {
+            this.printSolution((WeightedNode)n.getParent());
+        }
+
+        if (n.getPerformedOperation() != null) {
+            System.out.println(n.getPerformedOperation());
+        }
+
+    }
+
+    private Integer calcAStar(TableStatus st, WeightedNode n) {
+
+        return n.getPathLength() + st.diff(this.finalStatus);
+
+    }
+
+    private void performSolution(WeightedNode n) {
+
+        Collection<Operation> toDo = this.findWhatToDo(n.getCurrentStatus());
+
+        //expand all child WeightedNodes
+        for (Operation op : toDo) {
+            try {
+
+                TableStatus newStatus = op.performOperation();
+                WeightedNode child = new WeightedNode(newStatus, n, op, this.calcAStar(newStatus, n));
+                this.toExplore.add(child);
+
+                if (child.getCurrentStatus().equals(this.finalStatus)) {
+                    //found the leaf!
+                    this.solutionLeaf = child;
+                    return;
+                }
+
+            }
+            catch (CannotDoOperationException ex) {
+                System.out.println("\r\nTried to do an impossible operation : " + op);
+            }
+        }
+
+    }
+
+    /**
+     * Finds possible operations to do on a status.
+     * @param st The status to look at
+     * @return A list of operation to perform
+     */
+    private Collection<Operation> findWhatToDo(TableStatus st) {
+
+        HashMap<String, Operation> toDo = new HashMap<String, Operation>();
+
+        //scan all WeightedNodes till we find something to do
+        for (String nLabel : st.getArrangement().keySet()) {
+
+            //fix our "from" block
+            Block f = st.getArrangement().get(nLabel);
+
+            //find our m8 block
+            for (String tLabel : st.getArrangement().keySet()) {
+
+                if (nLabel == tLabel) {
+                    continue;
+                }
+
+                Block t = st.getArrangement().get(tLabel);
+                HashMap<String, Operation> toAdd = this.getPossibleOperations(st, f, t);
+                
+                for (String opS : toAdd.keySet()) {
+                    if (!toDo.containsKey(opS)) {
+                        toDo.put( opS, toAdd.get(opS) );
+                    }
+                }
+
+            }
+
+        }
+
+        return toDo.values();
+
+    }   
+    
+}
