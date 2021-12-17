2fffc0088ab46859bd6d6e6eb9c369ee1ff94ac3
zeffiro <r.zinni@wordpower.it>
2021-12-17 01:04:57 +0100

    Operation refactoring
    canDo transformed into a static method, the Operation instance contains previous status and block to move on table

--- blocksplanner/src/main/java/com/zeffiro/operations/MoveOn.java
+++ blocksplanner/src/main/java/com/zeffiro/operations/MoveOn.java
@@ -6,33 +6,38 @@ import com.zeffiro.exceptions.CannotDoOperationException;
 
 public class MoveOn extends Operation {
     
-    public MoveOn() {
-        super("Move on block");
+    public static String name = "Move on block";
+
+    private Block operatingBlockTo;
+
+    public MoveOn(TableStatus status, Block b, Block to) {
+        super(status, b);
+        this.operatingBlockTo = to;
     }
 
-    public boolean canDo(Block b) {
-        return super.canDo(b) && b.isFree() && !b.isOnTable();
+    public static boolean canDo(Block b, Block t) {
+        return Operation.canDo(b) && Operation.canDo(t) && b.isFree() && !b.isOnTable() && t.isFree();
     }
 
-    public TableStatus performOperation(TableStatus status, String labelFrom, String labelTo) throws CannotDoOperationException {
+    public TableStatus performOperation() throws CannotDoOperationException {
         
-        TableStatus pStatus = super.performOperation(status, labelFrom);
-
-        if (!pStatus.getArrangement().get(labelTo).isFree()) {
-            throw new CannotDoOperationException("Destination block is not free! Trying to '" + this + "' on block " + labelFrom + " to " + labelTo);
-        }
-
+        TableStatus pStatus = super.performOperation();
         TableStatus nStatus = new TableStatus(pStatus.getArrangement());
 
         //move block to another one
-        Block from = nStatus.getArrangement().get(labelFrom);
-        Block to = nStatus.getArrangement().get(labelTo);
+        Block from = nStatus.getArrangement().get(this.operatingBlock.getLabel());
+        Block to = nStatus.getArrangement().get(this.operatingBlockTo.getLabel());
 
         from.getUnder().setOver(null);
         to.setOver(from);
 
         return nStatus;
 
+    }
+
+    @Override
+    public String toString() {
+        return "Move " + this.operatingBlock.getLabel() + " on " + this.operatingBlockTo.getLabel();
     }
 
 }
